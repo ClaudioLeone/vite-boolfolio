7e40e0e18926b9d94c896bce993a998af1058f16
ClaudioLeone <claleone92@gmail.com>
2023-06-19 00:30:02 +0200

    Created 'Project Card' component and visualized on page with data from API backend

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <script>
 import axios from 'axios';
+// import {store} from './store';
+import ProjCard from './components/ProjCard.vue';
 
 export default {
-  data() {
-    return {
-      projects: []
-    }
-  },
-  mounted() {
-    this.getProjects();
-  },
-  methods: {
-    getProjects() {
-      axios.get('http://127.0.0.1:8000/api/projects').then((resp) => {
-        console.log(resp);
-      })
-    }
-  }
+    data() {
+        return {
+            projects: []
+        };
+    },
+    components: {
+      ProjCard
+    },
+    mounted() {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
+                this.projects = resp.data.results;
+            });
+        }
+    },
+    components: { ProjCard }
 }
 </script>
 
 <template>
-  <h1>TEST</h1>
+  <div class="container">
+    <h2 class="text-center my-3">Lista progetti</h2>
+    <div class="row row-cols-4 g-2">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjCard :project="project"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjCard.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        name: 'ProjCard',
+        props: {
+            project: Object
+        }
+    }
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body text-center bg-dark text-white">
+            <h6>{{ project.title }}</h6>
+            <p><strong>ID: </strong>{{ project.id }}</p>
+            <p><strong>TYPE: </strong>{{ project.type }}</p>
+            <p><strong>CONTENT: </strong>{{ project.content }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .card-body {
+        border-radius: 8px;
+        height: 375px;
+    }
+</style>
